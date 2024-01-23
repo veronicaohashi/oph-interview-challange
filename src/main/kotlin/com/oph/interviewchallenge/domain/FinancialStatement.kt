30f734e373175ace5c2444ed0d61f46a985a1c7b
veronica ohashi <veronica.ohashi@outlook.com>
2024-01-23 15:35:30 +0000

    fix: decimal calculation

--- src/main/kotlin/com/oph/interviewchallenge/domain/FinancialStatement.kt
+++ src/main/kotlin/com/oph/interviewchallenge/domain/FinancialStatement.kt
@@ -3,7 +3,10 @@ package com.oph.interviewchallenge.domain
 import com.oph.interviewchallenge.domain.transactions.Expenditure
 import com.oph.interviewchallenge.domain.transactions.Income
 import com.oph.interviewchallenge.domain.transactions.Transaction
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.util.UUID
 
 data class FinancialStatement(
@@ -12,13 +15,19 @@ data class FinancialStatement(
     val transactions: List<Transaction> = emptyList(),
     val rating: FinancialStatementRating? = null
 ) {
+    private var logger: Logger = LoggerFactory.getLogger(javaClass)
+
     fun updateTransaction(newTransactions: List<Transaction>) =
         this.copy(transactions = this.transactions + newTransactions)
 
     fun rating(): FinancialStatementRating {
         if (totalIncome() == BigDecimal.ZERO) return FinancialStatementRating.D
+        logger.info("TotalIncome: ${totalIncome()}")
+        logger.info("TotalExpenditure: ${totalExpenditure()}")
 
-        val ratio = totalExpenditure().divide(totalIncome()).toDouble()
+        val ratio = totalExpenditure().divide(totalIncome(), 2, RoundingMode.HALF_UP).toDouble()
+        logger.info("Ratio: $ratio")
+
         return when {
             ratio < 0.10 -> FinancialStatementRating.A
             ratio < 0.30 -> FinancialStatementRating.B
