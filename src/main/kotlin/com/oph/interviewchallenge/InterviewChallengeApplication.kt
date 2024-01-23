2ff2bdba8b93a2a02c3a9ce6ec18aec1ee1dc35c
veronica ohashi <veronica.ohashi@outlook.com>
2024-01-23 09:31:13 +0000

    initial commit

---
+++ src/main/kotlin/com/oph/interviewchallenge/InterviewChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.oph.interviewchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterviewChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/oph/interviewchallenge/InterviewChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oph.interviewchallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterviewChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
