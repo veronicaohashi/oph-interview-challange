772ab53266db793d863a215ecbb128c79cf4107a
veronica ohashi <veronica.ohashi@outlook.com>
2024-01-23 09:45:46 +0000

    chore: create transaction db entities

---
+++ src/main/kotlin/com/oph/interviewchallenge/infraestructure/entities/IncomeEntity.kt
@@ -0,0 +1,25 @@
+package com.oph.interviewchallenge.infraestructure.entities
+
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Table(name = "incomes")
+@Entity
+class IncomeEntity(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    @Column(name = "description", nullable = false, updatable = false)
+    val description: String,
+
+    @Column(name = "amount", nullable = false, updatable = false)
+    val amount: BigDecimal,
+
+    @Column(name = "date", nullable = false, updatable = false)
+    val date: LocalDate,
+)
